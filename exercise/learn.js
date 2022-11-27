43beb7ef81ddbe569654e1a5d474c5c20e606e2c
mehraj-hussain <mhussainceo@gmail.com>
2022-11-27 23:09:02 +0530

    tree

---
+++ exercise/learn.js
@@ -0,0 +1,16 @@
+var readlineSync=require("readline-sync")
+
+// var a;
+// a = readlineSync.question("Please Enter your Name : ");
+
+// //Take String Input
+// var a = readlineSync.question("Please Enter your Name : ");
+
+console.log("    + ")
+console.log("   +**")
+console.log("  ****+")
+console.log(" *+**+**")
+console.log("*+****+**")
+console.log("    |")
+// console.log("\tAdd TWO NUMBERS")
+console.log("   ===")
